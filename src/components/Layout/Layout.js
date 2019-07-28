54efdf56baa0b1fde42b0199481bbcd2aa3eb448
yvenencia <yeissonvenencia@gmail.com>
2019-07-28 11:37:41 -0400

    restructuring the project folders

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Auxiliary from '../../hoc/Auxiliary'
+
+const Layout = (props) => {
+    return (
+        <Auxiliary>
+            <div>
+                Toolbar, sideDrawer, Backdrop
+            </div>
+            <main>
+                {props.children}
+            </main>
+        </Auxiliary>
+    )
+}
+
+export default Layout
---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../styles/styles.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default App;
